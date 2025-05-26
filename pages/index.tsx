6121797bc897fa309b73b59b8a99bccfc228b751
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-26 09:41:27 +0900

    add footer and index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 
 import Header from "@/components/header/Header";
 import CustomSlider from "@/components/slider/Slider";
-import LoginPage from "@/features/login/LoginPage";
 
 export default function Home() {
     return (
         <>
             <Header />
-            {/* <LoginPage /> */}
             <div className="w-full">
                 <CustomSlider />
             </div>
