b9ec79f499927ab955d49aa6ac286d45a8296795
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-30 16:47:24 +0900

    fix:google map fix

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,16 +1,17 @@
-
-
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-export default function LeafletMap() {
+const GoogleMap = () => {
+    const googleMapsUrl = `https://mt1.google.com/vt/lyrs=m@221097413,traffic&x={x}&y={y}&z={z}&token=96706`;
     return (
-        <MapContainer
-            center={[35.3607411, 138.727262]}
-            zoom={13}
-            scrollWheelZoom={false}
-            style={{ height: '100vh', width: '100%' }}
-        >
+        <MapContainer center={[34.0522, -118.2437]} zoom={10} style={{ height: '500px', width: '100%' }}>
+            <TileLayer
+                url={googleMapsUrl}
+                attribution='&copy; Google Maps'
+            />
         </MapContainer>
     );
-}
+};
+
+export default GoogleMap;
