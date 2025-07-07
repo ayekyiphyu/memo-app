e0a95f98f74dc8a04e0d25ae200779510c01b5f5
ayekyiphyu <ayekyiphyu@yuime.co.jp>
2025-07-07 14:53:21 +0900

    fix:gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,30 @@
-import type { NextConfig } from "next";
-import path from "path";
+// import type { NextConfig } from "next";
+// import path from "path";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   trailingSlash: true,
+//   appDir: true,
+//   devIndicators: false,
+//   turbopack: {},
+//   webpack: (config) => {
+//     config.resolve.alias["@components"] = path.join(__dirname, "components");
+//     config.resolve.alias["@styles"] = path.join(__dirname, "styles");
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   trailingSlash: true,
-  appDir: true,
-  devIndicators: false,
-  turbopack: {},
-  webpack: (config) => {
-    config.resolve.alias["@components"] = path.join(__dirname, "components");
-    config.resolve.alias["@styles"] = path.join(__dirname, "styles");
-    return config;
+  images: {
+    unoptimized: true,
   },
+  basePath: "/memo-app", // Replace 'memo-app' with your repository name
+  assetPrefix: "/memo-app/", // Replace 'memo-app' with your repository name
 };
 
 module.exports = nextConfig;
