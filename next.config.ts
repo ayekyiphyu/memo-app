33d4be12dd4dec4cfbe0a133b44094f38e822270
ayekyiphyu <ayekyiphyu@yuime.co.jp>
2025-07-07 15:17:54 +0900

    fix:next.config

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,15 @@
-// import type { NextConfig } from "next";
-// import path from "path";
+import type { NextConfig } from "next";
+import path from "path";
 
-// const nextConfig: NextConfig = {
-//   output: "export",
-//   trailingSlash: true,
-//   appDir: true,
-//   devIndicators: false,
-//   turbopack: {},
-//   webpack: (config) => {
-//     config.resolve.alias["@components"] = path.join(__dirname, "components");
-//     config.resolve.alias["@styles"] = path.join(__dirname, "styles");
-//     return config;
-//   },
-// };
-
-// module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
+  appDir: true,
+  devIndicators: false,
+  webpack: (config) => {
+    config.resolve.alias["@components"] = path.join(__dirname, "components");
+    config.resolve.alias["@styles"] = path.join(__dirname, "styles");
+    return config;
   },
-  basePath: "/memo-app", // Replace 'memo-app' with your repository name
-  assetPrefix: "/memo-app/", // Replace 'memo-app' with your repository name
 };
 
 module.exports = nextConfig;
