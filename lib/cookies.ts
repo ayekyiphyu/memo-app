f03f1cc84edca3bf5cc90bd381c4b584e5aaef10
ayekyiphyu <ayekyiphyu@yuime.co.jp>
2025-07-08 17:11:04 +0900

    fix:remove to lib/cookies.ts file

--- lib/cookies.ts
+++
@@ -1,7 +0,0 @@
-export default function getCookie(name: string) {
-    if (typeof document === 'undefined') return null;
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
-    return null;
-}
